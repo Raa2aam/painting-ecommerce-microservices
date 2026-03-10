067ba0c86169098907bcd85efbdade3c9c1929a9
Raa2aam <ramchandra134454@gmail.com>
2026-03-10 08:47:04 +0530

    painting service initial changes
    painting service initial changes

---
+++ painting-service/src/main/java/com/indianpaint/paintingservice/PaintingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.indianpaint.paintingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaintingServiceApplication.class, args);
+	}
+
+}
---
+++ painting-service/src/test/java/com/indianpaint/paintingservice/PaintingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indianpaint.paintingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
